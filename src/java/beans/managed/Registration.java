ab3a70fedb38dd5fe11df47f2fa9a6bb3397cf56
Towe1ie <lucic_92@hotmail.com>
2016-07-02 05:19:08 +0200

    registracija radi sa sve slikom!!!

--- src/java/beans/managed/Registration.java
+++ src/java/beans/managed/Registration.java
@@ -5,15 +5,35 @@
  */
 package beans.managed;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.*;
+import javax.servlet.http.Part;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.DefaultUploadedFile;
+import org.primefaces.model.UploadedFile;
 
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class Registration
 {
+
     private String username, password, firstName, lastName, phone, email, organization;
     private Date dateOfBirth;
+    private Part image;
+    private UploadedFile file;
 
     // <editor-fold defaultstate="collapsed" desc="Getters and setters">
     public String getFirstName()
@@ -24,6 +44,16 @@ public class Registration
     public void setFirstName(String firstName)
     {
 	this.firstName = firstName;
+    }
+
+    public UploadedFile getFile()
+    {
+	return file;
+    }
+
+    public void setFile(UploadedFile file)
+    {
+	this.file = file;
     }
 
     public String getLastName()
@@ -95,12 +125,54 @@ public class Registration
     {
 	this.password = password;
     }
-    
+
+    public Part getImage()
+    {
+	return image;
+    }
+
+    public void setImage(Part image)
+    {
+	this.image = image;
+    }
     // </editor-fold>
-    
+
+    public void imageUploaded(FileUploadEvent e)
+    {
+	file = e.getFile();
+    }
+
     public String register()
     {
-	
+	try (InputStream image_input = file.getInputstream())
+	{
+	    Class.forName("com.mysql.jdbc.Driver");
+	    Connection conn = DriverManager.getConnection(
+		    "jdbc:mysql://localhost:3306/pia",
+		    "Nemanja",
+		    "Olovcica1");
+
+	    String query = "INSERT INTO user_accounts values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	    PreparedStatement ps = conn.prepareStatement(query);
+
+	    ps.setString(1, username);
+	    ps.setString(2, password);
+	    ps.setString(3, firstName);
+	    ps.setString(4, lastName);
+	    ps.setString(5, phone);
+	    ps.setString(6, email);
+	    ps.setString(7, organization);
+	    ps.setDate(8, new java.sql.Date(dateOfBirth.getTime()));
+	    ps.setBinaryStream(9, image_input);
+	    
+	    ps.executeUpdate();
+	}
+	catch (Exception ex)
+	{
+	    String msg = ex.getMessage();
+	    System.out.println(msg);
+	}
+
 	return null;
     }
 }
--- web/registration.js
+++ web/registration.js
@@ -7,6 +7,22 @@
 var bad_color = "#ff6666";
 var ok_color = "#66cc66";
 
+var pass1_ok, pass2_ok, firstName_ok, email_ok, phone_ok;
+
+function checkAll()
+{
+    console.log("CHECK ALL");
+    checkPassword1();
+    checkPassword2();
+    checkFirstName();
+    checkLastName();
+    checkEmail();
+    checkPhone();
+    return false;
+}
+
+window.onload = checkAll();
+
 function checkPassword1()
 {
     var pass_elem = document.getElementById("id_regform:id_pass1");
@@ -18,12 +34,14 @@ function checkPassword1()
 	pass_elem.style.backgroundColor = ok_color;
 	msg_elem.innerHTML = "Pasword is fine";
 	msg_elem.style.color = ok_color;
+	pass1_ok = true;
     }
     else
     {
 	pass_elem.style.backgroundColor = bad_color;
 	msg_elem.innerHTML = "Pasword must containt at least one small letter, one big letter and one number";
 	msg_elem.style.color = bad_color;
+	pass1_ok = false;
     }
 
 }
@@ -35,16 +53,111 @@ function checkPassword2()
     var pass2 = pass2_elem.value;
     var msg_elem = document.getElementById("id_pass2_msg");
 
-    if (pass1 === pass2)
+    if (pass1 === pass2 && pass1_ok)
     {
 	pass2_elem.style.backgroundColor = ok_color;
 	msg_elem.innerHTML = "Password matches original";
 	msg_elem.style.color = ok_color;
+	pass2_ok = true;
     }
     else
     {
 	pass2_elem.style.backgroundColor = bad_color;
 	msg_elem.innerHTML = "Password doesnt match original";
 	msg_elem.style.color = bad_color;
+	pass2_ok = false;
+    }
+}
+
+function checkFirstName()
+{
+    var name_elem = document.getElementById("id_regform:id_firstName");
+    var patt = new RegExp(/([^a-zA-Z])/);
+    var msg_elem = document.getElementById("id_firstName_msg");
+
+    var len = name_elem.value.toString().length;
+
+    if (!patt.test(name_elem.value) && len >= 2)
+    {
+	name_elem.style.backgroundColor = ok_color;
+	msg_elem.innerHTML = "First name fine";
+	msg_elem.style.color = ok_color;
+	firstName_ok = true;
+    }
+    else
+    {
+	name_elem.style.backgroundColor = bad_color;
+	msg_elem.innerHTML = "First name must be at least two characters and can containt only letters";
+	msg_elem.style.color = bad_color;
+	firstName_ok = false;
+    }
+}
+
+function checkLastName()
+{
+    var name_elem = document.getElementById("id_regform:id_lastName");
+    var patt = new RegExp(/([^a-zA-Z])/);
+    var msg_elem = document.getElementById("id_lastName_msg");
+
+    var len = name_elem.value.toString().length;
+
+    if (!patt.test(name_elem.value) && len >= 2)
+    {
+	name_elem.style.backgroundColor = ok_color;
+	msg_elem.innerHTML = "Last name fine";
+	msg_elem.style.color = ok_color;
+	lastName_ok = true;
+    }
+    else
+    {
+	name_elem.style.backgroundColor = bad_color;
+	msg_elem.innerHTML = "Last name must be at least two characters and can containt only letters";
+	msg_elem.style.color = bad_color;
+	lastName_ok = false;
+    }
+}
+
+function checkPhone()
+{
+    var phone_elem = document.getElementById("id_regform:id_phone");
+    var patt = new RegExp(/[^0-9]/);
+    var phone_msg = document.getElementById("id_phone_msg");
+
+    if (!patt.test(phone_elem.value) && phone_elem.value.length >= 9)
+    {
+	phone_elem.style.backgroundColor = ok_color;
+	phone_msg.innerHTML = "Valid number";
+	phone_msg.style.color = ok_color;
+	phone_ok = true;
+    }
+    else
+    {
+	phone_elem.style.backgroundColor = bad_color;
+	phone_msg.innerHTML = "Invalid number";
+	phone_msg.style.color = bad_color;
+	phone_ok = false;
+    }
+}
+
+function checkEmail()
+{
+    var email_elem = document.getElementById("id_regform:id_email");
+    var patt = new RegExp(/\S+@\S+(\.\S+)$/);
+
+    var email_msg = document.getElementById("id_email_msg");
+
+    if (patt.test(email_elem.value))
+    {
+	email_elem.style.backgroundColor = ok_color;
+	email_msg.innerHTML = "Valid email";
+	email_msg.style.color = ok_color;
+	email_ok = true;
+    }
+    else
+    {
+	email_elem.style.backgroundColor = bad_color;
+	email_msg.innerHTML = "Invalid email";
+	email_msg.style.color = bad_color;
+	email_ok = false;
     }
 }
